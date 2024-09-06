67e18d393a6482664f6183abb415fed30b720529
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-05 23:52:29 -0300

    test: add user entity unit test

---
+++ tests/layers/domain/user/user.entity.spec.ts
@@ -0,0 +1,205 @@
+import { UserEntity, DomainError, InvalidUserEmailError, InvalidUserPasswordError, InvalidUsernameError } from "@/layers/domain";
+
+describe(("Entity - User"), () => {
+    
+	test("Should not create user, because user email is not valid" , () => {
+		const invalidUserEmail = "email.com";
+		const username = "username";
+		const password = "Password1234";
+
+		const sut = (): UserEntity => new UserEntity({
+			email: invalidUserEmail, 
+			username: username, 
+			password,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		expect(sut).toThrow(DomainError);
+	});
+
+	test("Should not create user, because username is not valid" , () => {
+		const userEmail = "email@test.com";
+		const invalidUsername = "";
+		const password = "password";
+
+		const sut = (): UserEntity => new UserEntity({
+			email: userEmail, 
+			username: invalidUsername, 
+			password,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		expect(sut).toThrow(DomainError);
+	});
+
+	test("Should not create user, because user password is not valid" , () => {
+		const email = "email@test.com";
+		const username = "username";
+		const invalidUserPassword = "password";
+
+		const sut = (): UserEntity => new UserEntity({
+			email: email, 
+			username: username, 
+			password: invalidUserPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		expect(sut).toThrow(DomainError);
+	});
+
+	test("Should create user" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+
+		const sut = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		expect(sut).toBeInstanceOf(UserEntity);
+	});
+
+	test("Should not update email, because email is invalid" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		const sut = (): string => user.email = "";
+
+		expect(sut).toThrow(InvalidUserEmailError);
+	});
+
+
+	test("Should update email" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		user.email = "email2@test.com";
+
+		expect(user.email).toBe("email2@test.com");
+	});
+
+	test("Should not update username, because username is invalid" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		const sut = (): string => user.username = "";
+
+		expect(sut).toThrow(InvalidUsernameError);
+	});
+
+	test("Should update username" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		user.username = "username 2";
+
+		expect(user.username).toBe("username 2");
+	});
+
+	test("Should not update password, because password is invalid" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		const sut = (): string => user.password = "";
+
+		expect(sut).toThrow(InvalidUserPasswordError);
+	});
+
+	test("Should update password" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		user.password = "Password12345";
+
+		expect(user.password).toBe("Password12345");
+	});
+
+	test("Should not update verified email, because verified email already is true" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: true,
+			createdAt: new Date()
+		});
+
+		const sut = (): boolean => user.verifiedEmail = true;
+
+		expect(sut).toThrow(DomainError);
+	});
+
+	test("Should update verified email" , () => {
+		const userEmail = "email@test.com";
+		const username = "username";
+		const userPassword = "Password1234";
+		const user = new UserEntity({
+			email: userEmail, 
+			username: username, 
+			password: userPassword,
+			verifiedEmail: false,
+			createdAt: new Date()
+		});
+
+		user.verifiedEmail = true;
+
+		expect(user.verifiedEmail).toBeTruthy();
+	});
+});

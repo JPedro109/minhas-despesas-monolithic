adb274ce32fc2d7ce695e7572c7ca36a888cd866
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-08 02:30:18 -0300

    feat: add expense entity

---
+++ src/layers/domain/entities/expense/expense.entity.ts
@@ -0,0 +1,79 @@
+import { AbstractEntity } from "../abstract/abstract.entity";
+import { ExpenseNameValueObject, ExpenseValueValueObject, ExpenseDueDateValueObject, DomainError } from "@/layers/domain";
+
+export type ExpenseProps = {
+    enpenseName: string;
+    expenseValue: number;
+    dueDate: Date;
+    paid: boolean;
+    createdAt: Date;
+	updatedAt?: Date;
+}
+
+export class ExpenseEntity extends AbstractEntity<ExpenseProps> {
+
+    constructor(props: ExpenseProps, id?: string) {
+		super(props, id);
+
+		const valueObjets = {
+			enpenseName: ExpenseNameValueObject.create(props.enpenseName),
+			expenseValue: ExpenseValueValueObject.create(props.expenseValue),
+			dueDate: ExpenseDueDateValueObject.create(props.dueDate),
+		};
+
+		const result = ExpenseEntity.validate(valueObjets);
+
+		if(!result.valid) throw new DomainError(result.errors);
+	}
+
+    get expenseName(): string {
+        return this.props.enpenseName;
+    }
+
+    set expenseName(expenseName: string) {
+        const result = ExpenseNameValueObject.create(expenseName);
+		if (result instanceof Error) throw result;
+        this.props.enpenseName = result.value;
+    }
+
+    get expenseValue(): number {
+        return this.props.expenseValue;
+    }
+
+    set expenseValue(expenseValue: number) {
+        const result = ExpenseValueValueObject.create(expenseValue);
+		if (result instanceof Error) throw result;
+        this.props.expenseValue = result.value;
+    }
+
+    get dueDate(): Date {
+        return this.props.dueDate;
+    }
+
+    set dueDate(dueDate: Date) {
+        const result = ExpenseDueDateValueObject.create(dueDate);
+		if (result instanceof Error) throw result;
+        this.props.dueDate = result.value;
+    }
+
+    get paid(): boolean {
+        return this.props.paid;
+    }
+
+    set paid(paid: boolean) {
+		if(this.props.paid === paid) throw new DomainError(`A despesa já está ${paid ? "paga" : "como não paga"}`);
+        this.props.paid = paid;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
+    get updatedAt(): Date {
+        return this.props.updatedAt;
+    }
+
+    set updatedAt(updatedAt: Date) {
+        this.props.updatedAt = updatedAt;
+    }
+}
--- src/layers/domain/entities/expense/index.ts
+++ src/layers/domain/entities/expense/index.ts
@@ -6,3 +6,5 @@ export * from "./errors/invalid-expense-value.error";
 
 export * from "./expense-due-date.value-object";
 export * from "./errors/invalid-expense-due-date.error";
+
+export * from "./expense.entity";

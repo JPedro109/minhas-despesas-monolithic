308cbd934726191fc20776023b3e604b4636b0db
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-08 23:54:24 -0300

    test: add user verification code value object unit test

---
+++ tests/layers/domain/user-verification-code/user-verification-code.value-object.spec.ts
@@ -0,0 +1,28 @@
+import { UserVerificationCodeValueObject, InvalidUserVerificationCodeError } from "@/layers/domain";
+
+describe(("Object Value - UserVerificationCodeValueObject"), () => {
+    
+	test("Should not create user verification code, because user verification code is empty" , () => {
+		const invalidUserVerificationCode = "";
+
+		const sut = UserVerificationCodeValueObject.create(invalidUserVerificationCode);
+
+		expect(sut).toBeInstanceOf(InvalidUserVerificationCodeError);
+	});
+
+	test("Should not create user verification code, because the user verification code has more than 6 characters" , () => {
+		const invalidUserVerificationCode = "c".repeat(7);
+
+		const sut = UserVerificationCodeValueObject.create(invalidUserVerificationCode);
+
+		expect(sut).toBeInstanceOf(InvalidUserVerificationCodeError);
+	});
+
+	test("Should not create user verification code, because the user verification code has more than 256 characters" , () => {
+		const userVerificationCode = "123456";
+
+		const sut = UserVerificationCodeValueObject.create(userVerificationCode);
+
+		expect(sut).toBeInstanceOf(UserVerificationCodeValueObject);
+	});
+});

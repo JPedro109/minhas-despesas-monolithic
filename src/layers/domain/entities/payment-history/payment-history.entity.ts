47532d23ffe2f5193cabe63cb9830328581e541a
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-10 00:25:33 -0300

    feat: add payment history entity

---
+++ src/layers/domain/entities/payment-history/payment-history.entity.ts
@@ -0,0 +1,46 @@
+import { AbstractEntity } from "../abstract/abstract.entity";
+
+export type PaymentHistoryProps = {
+    userId: string;
+    expenseId: string;
+    expenseName: string;
+    expenseValue: number;
+    dueDate: Date;
+    paidDate: Date;
+    createdAt: Date;
+}
+
+export class PaymentHistoryEntity extends AbstractEntity<PaymentHistoryProps> {
+
+    constructor(props: PaymentHistoryProps, id?: string) {
+        super(props, id);
+    }
+
+    get userId(): string {
+        return this.props.userId;
+    }
+
+    get expenseId(): string {
+        return this.props.expenseId;
+    }
+
+    get expenseName(): string {
+        return this.props.expenseName;
+    }
+
+    get expenseValue(): number {
+        return this.props.expenseValue;
+    }
+
+    get dueDate(): Date {
+        return this.props.dueDate;
+    }
+
+    get paidDate(): Date {
+        return this.props.paidDate;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+}
--- src/layers/domain/index.ts
+++ src/layers/domain/index.ts
@@ -3,3 +3,4 @@ export * from "./errors/domain.error";
 export * from "./entities/user";
 export * from "./entities/user-verification-code";
 export * from "./entities/expense";
+export * from "./entities/payment-history";

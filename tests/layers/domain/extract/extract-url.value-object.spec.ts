ae9df442f1d932dfcf1a502a1a734f940248cdf5
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-10 01:21:30 -0300

    test: add extract url value object unit test

---
+++ tests/layers/domain/extract/extract-url.value-object.spec.ts
@@ -0,0 +1,45 @@
+import { ExtractUrlValueObject, InvalidExtractUrlError } from "@/layers/domain";
+
+describe("Object Value - ExtractUrlValueObject", () => {
+
+    test("Should not create URL, because URL is empty", () => {
+        const invalidUrl = "";
+
+        const sut = ExtractUrlValueObject.create(invalidUrl);
+
+        expect(sut).toBeInstanceOf(InvalidExtractUrlError);
+    });
+
+    test("Should not create URL, because URL is invalid", () => {
+        const invalidUrl = "htp://invalid-url";
+
+        const sut = ExtractUrlValueObject.create(invalidUrl);
+
+        expect(sut).toBeInstanceOf(InvalidExtractUrlError);
+    });
+
+    test("Should not create URL, because URL has invalid scheme", () => {
+        const invalidUrl = "ftp://example.com";
+
+        const sut = ExtractUrlValueObject.create(invalidUrl);
+
+        expect(sut).toBeInstanceOf(InvalidExtractUrlError);
+    });
+
+    test("Should not create URL, because URL is too long", () => {
+        const invalidUrl = "https://" + "a".repeat(2048);
+
+        const sut = ExtractUrlValueObject.create(invalidUrl);
+
+        expect(sut).toBeInstanceOf(InvalidExtractUrlError);
+    });
+
+    
+    test("Should create URL, because URL is valid", () => {
+        const url = "https://www.example.com";
+
+        const sut = ExtractUrlValueObject.create(url);
+
+        expect(sut).toBeInstanceOf(ExtractUrlValueObject);
+    });
+});

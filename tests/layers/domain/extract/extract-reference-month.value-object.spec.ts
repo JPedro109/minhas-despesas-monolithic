5a25203ca57eec07cabb56efb66be93e8b8db11d
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-16 21:14:10 -0300

    test: add extract reference month value object unit test

---
+++ tests/layers/domain/extract/extract-reference-month.value-object.spec.ts
@@ -0,0 +1,37 @@
+import { ExtractReferenceMonthValueObject, InvalidExtractReferenceMonthError } from "@/layers/domain";
+
+describe("Object Value - ExtractReferenceMonthValueObject", () => {
+
+    test("Should not create reference month, because reference month is empty", () => {
+        const invalidReferenceMonth = null;
+
+        const sut = ExtractReferenceMonthValueObject.create(invalidReferenceMonth);
+
+        expect(sut).toBeInstanceOf(InvalidExtractReferenceMonthError);
+    });
+
+    test("Should not create reference month, because reference month is less than 1", () => {
+        const invalidReferenceMonth = 0;
+
+        const sut = ExtractReferenceMonthValueObject.create(invalidReferenceMonth);
+
+        expect(sut).toBeInstanceOf(InvalidExtractReferenceMonthError);
+    });
+
+    test("Should not create reference month, because reference month is greater than 12", () => {
+        const invalidReferenceMonth = 13;
+
+        const sut = ExtractReferenceMonthValueObject.create(invalidReferenceMonth);
+
+        expect(sut).toBeInstanceOf(InvalidExtractReferenceMonthError);
+    });
+
+    
+    test("Should create reference month, because reference month is valid", () => {
+        const referenceMonth = 1;
+
+        const sut = ExtractReferenceMonthValueObject.create(referenceMonth);
+
+        expect(sut).toBeInstanceOf(ExtractReferenceMonthValueObject);
+    });
+});

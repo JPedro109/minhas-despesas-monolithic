b49e48ef7fcc5f261eab1158f175912f999f555e
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-16 21:31:37 -0300

    test: add extract entity unit test

---
+++ tests/layers/domain/extract/extract.entity.spec.ts
@@ -0,0 +1,44 @@
+import { ExtractEntity, DomainError } from "@/layers/domain";
+
+describe("Entity - Extract", () => {
+
+  test("Should not create ExtractEntity, because URL is invalid", () => {
+    const invalidUrl = "invalid-url";
+    const referenceMonth = 1;
+
+    const sut = (): ExtractEntity => new ExtractEntity({
+      url: invalidUrl,
+      referenceMonth,
+      createdAt: new Date()
+    });
+
+    expect(sut).toThrow(DomainError);
+  });
+
+  test("Should not create ExtractEntity, because reference month is invalid", () => {
+    const url = "https://example.com";
+    const invalidReferenceMonth = -1;
+
+    const sut = (): ExtractEntity => new ExtractEntity({
+      url,
+      referenceMonth: invalidReferenceMonth,
+      createdAt: new Date()
+    });
+
+    expect(sut).toThrow(DomainError);
+  });
+
+
+  test("Should create ExtractEntity", () => {
+    const validUrl = "https://example.com";
+    const referenceMonth = 1;
+
+    const sut = new ExtractEntity({
+      url: validUrl,
+      referenceMonth,
+      createdAt: new Date()
+    });
+
+    expect(sut).toBeInstanceOf(ExtractEntity);
+  });
+});

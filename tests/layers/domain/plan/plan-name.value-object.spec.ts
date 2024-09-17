71669b8af7dc240d9fb063deef46c34ee942c917
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-16 23:56:41 -0300

    test: add plan name value object unit test

---
+++ tests/layers/domain/plan/plan-name.value-object.spec.ts
@@ -0,0 +1,28 @@
+import { PlanNameValueObject, InvalidPlanNameError } from "@/layers/domain";
+
+describe(("Object Value - PlanNameValueObject"), () => {
+    
+	test("Should not create plan name, because plan name is empty" , () => {
+		const invalidUsername = "";
+
+		const sut = PlanNameValueObject.create(invalidUsername);
+
+		expect(sut).toBeInstanceOf(InvalidPlanNameError);
+	});
+
+	test("Should not create plan name, because the plan name has more than 51 characters" , () => {
+		const invalidUsername = "c".repeat(51);
+
+		const sut = PlanNameValueObject.create(invalidUsername);
+
+		expect(sut).toBeInstanceOf(InvalidPlanNameError);
+	});
+
+	test("Should create plan name" , () => {
+		const planName = "plan name";
+
+		const sut = PlanNameValueObject.create(planName);
+
+		expect(sut).toBeInstanceOf(PlanNameValueObject);
+	});
+});

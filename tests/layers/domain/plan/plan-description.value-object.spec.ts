bb1a44253081b31c81456d6818ad3bc5cb548667
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-17 00:09:09 -0300

    test: add plan description value object unit test

---
+++ tests/layers/domain/plan/plan-description.value-object.spec.ts
@@ -0,0 +1,28 @@
+import { PlanDescriptionValueObject, InvalidPlanDescriptionError } from "@/layers/domain";
+
+describe(("Object Value - PlanDescriptionValueObject"), () => {
+    
+	test("Should not create plan description, because plan description is empty" , () => {
+		const invalidPlanDescription = "";
+
+		const sut = PlanDescriptionValueObject.create(invalidPlanDescription);
+
+		expect(sut).toBeInstanceOf(InvalidPlanDescriptionError);
+	});
+
+	test("Should not create plan description, because the plan description has more than 100 characters" , () => {
+		const invalidPlanDescription = "c".repeat(101);
+
+		const sut = PlanDescriptionValueObject.create(invalidPlanDescription);
+
+		expect(sut).toBeInstanceOf(InvalidPlanDescriptionError);
+	});
+
+	test("Should create plan description" , () => {
+		const planName = "plan description";
+
+		const sut = PlanDescriptionValueObject.create(planName);
+
+		expect(sut).toBeInstanceOf(PlanDescriptionValueObject);
+	});
+});

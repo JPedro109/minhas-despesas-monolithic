6f3965bd1e444ffd8f869fd15b6f04755513779f
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-19 20:05:08 -0300

    test: add subscription amount value object unit test

---
+++ tests/layers/domain/subscription/subscription-amount.value-object.spec.ts
@@ -0,0 +1,21 @@
+import { SubscriptionAmountValueObject, InvalidSubscriptionAmountError } from "@/layers/domain";
+
+describe("Value Object - SubscriptionAmountValueObject", () => {
+
+    test("Should not create SubscriptionAmountValueObject, because value is less than zero", () => {
+        const invalidExpenseValue = -1;
+      
+        const sut = SubscriptionAmountValueObject.create(invalidExpenseValue);
+      
+        expect(sut).toBeInstanceOf(InvalidSubscriptionAmountError);
+    });
+  
+    test("Should create SubscriptionAmountValueObject", () => {
+      const expenseValue = 1;
+    
+      const sut = SubscriptionAmountValueObject.create(expenseValue) as unknown as SubscriptionAmountValueObject;
+    
+      expect(sut).toBeInstanceOf(SubscriptionAmountValueObject);
+      expect(sut.value).toBe(expenseValue);
+    });
+});

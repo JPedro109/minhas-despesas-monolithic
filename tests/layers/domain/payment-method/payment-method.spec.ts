fcfadad01f9afc3d0fb860ab78ae68a6ae589ced
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-09-30 19:20:05 -0300

    test: add payment method unit test

---
+++ tests/layers/domain/payment-method/payment-method.spec.ts
@@ -0,0 +1,26 @@
+import { PaymentMethodEntity } from "@/layers/domain";
+
+describe("Entity - PaymentMethod", () => {
+
+    test("Should create PaymentMethodEntity", () => {
+        const userId = "1";
+        const name = "Card One";
+        const token = "card_one_1";
+        const updatedAt = new Date();
+        
+        const sut = new PaymentMethodEntity({
+            userId,
+            name,
+            token,
+            updatedAt
+        });
+
+        expect(sut).toBeInstanceOf(PaymentMethodEntity);
+        expect(sut.id).not.toBeUndefined();
+        expect(sut.userId).toBe(userId);
+        expect(sut.name).toBe(name);
+        expect(sut.token).toBe(token);
+        expect(sut.createdAt).not.toBeUndefined();
+        expect(sut.updatedAt).not.toBeUndefined();
+    });
+});

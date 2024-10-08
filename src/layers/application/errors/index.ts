b70ba4f8f32ac75e46b3c90f02a8c4b14b215932
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-07 22:29:53 -0300

    feat: add not found error

--- src/layers/application/errors/index.ts
+++ src/layers/application/errors/index.ts
@@ -1,3 +1,5 @@
 export * from "./conflicted-error";
 export * from "./forbidden-error";
 export * from "./invalid-param-error";
+export * from "./not-found-error";
+export * from "./unauthorized-error";

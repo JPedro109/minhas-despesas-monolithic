b0c548d1ebb9227458ef09f6959bec9ea6759c5b
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-09 00:07:26 -0300

    refactor: rename files

--- src/layers/application/use-cases/user/index.ts
+++ src/layers/application/use-cases/user/index.ts
@@ -1,7 +1,7 @@
 export * from "./create-user/create-user.dtos";
-export * from "./create-user/create-user.interface";
+export * from "./create-user/create-user-use-case.interface";
 export * from "./create-user/create-user.use-case";
 
 export * from "./delete-user/delete-user.dtos";
-export * from "./delete-user/delete-user.interface";
+export * from "./delete-user/delete-user-use-case.interface";
 export * from "./delete-user/delete-user.use-case";

b79f8a88abe3aa055c24584260e47632d94e2fb9
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-12 03:13:58 -0300

    test: add update username use case unit test

---
+++ tests/layers/application/user/update-username.use-case.spec.ts
@@ -0,0 +1,64 @@
+import {
+    CustomerRepositoryStub,
+    PlanRepositoryStub,
+    SubscriptionRepositoryStub,
+    UnitOfWorkRepositoryStub,
+    UserConsentRepositoryStub,
+    UserRepositoryStub,
+    UserVerificationCodeRepositoryStub
+} from "../__mocks__";
+import {
+    NotFoundError,
+    UpdateUsernameUseCase
+} from "@/layers/application";
+
+const makeSut = (): {
+    sut: UpdateUsernameUseCase,
+    userRepositoryStub: UserRepositoryStub
+} => {
+    const userRepositoryStub = new UserRepositoryStub();
+    const userConsentRepositoryStub = new UserConsentRepositoryStub();
+    const userVerificationCodeRepositoryStub = new UserVerificationCodeRepositoryStub();
+    const customerRepositoryStub = new CustomerRepositoryStub();
+    const planRepositoryStub = new PlanRepositoryStub();
+    const subscriptionRepositoryStub = new SubscriptionRepositoryStub();
+    const unitOfWorkRepositoryStub = new UnitOfWorkRepositoryStub(
+        userRepositoryStub,
+        userVerificationCodeRepositoryStub,
+        customerRepositoryStub,
+        planRepositoryStub,
+        subscriptionRepositoryStub,
+        userConsentRepositoryStub
+    );
+    const sut = new UpdateUsernameUseCase(unitOfWorkRepositoryStub);
+
+    return {
+        sut,
+        userRepositoryStub
+    };
+};
+
+describe("Use case - UpdateUsernameUseCase", () => {
+    test("Should throw error if user does not exist", async () => {
+        const { sut, userRepositoryStub } = makeSut();
+        jest.spyOn(userRepositoryStub, "getUserById").mockReturnValueOnce(null);
+
+        const result = sut.execute({
+            id: "2",
+            username: "new-username"
+        });
+
+        await expect(result).rejects.toThrow(NotFoundError);
+    });
+
+    test("Should update username successfully", async () => {
+        const { sut } = makeSut();
+
+        const result = await sut.execute({
+            id: "1",
+            username: "new-username"
+        });
+
+        expect(result).toBe("email@teste.com");
+    });
+});

cd4e674bcfba7f46fc00022338d2da34d159e774
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-14 14:30:15 -0300

    refactor: standardize file export

--- src/layers/domain/index.ts
+++ src/layers/domain/index.ts
@@ -1,12 +1,2 @@
 export * from "./errors";
-
-export * from "./entities/user";
-export * from "./entities/user-consent";
-export * from "./entities/customer";
-export * from "./entities/payment-method";
-export * from "./entities/user-verification-code";
-export * from "./entities/expense";
-export * from "./entities/payment-history";
-export * from "./entities/extract";
-export * from "./entities/plan";
-export * from "./entities/subscription";
+export * from "./entities";

f1a1c628363cd645edd9d6f256ef70d3bc104a0a
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-14 15:06:09 -0300

    test: add get plan user use case unit test

---
+++ tests/layers/application/plan/get-user-plan.use-case.spec.ts
@@ -0,0 +1,44 @@
+import {
+    unitOfWorkRepositoryStub,
+    SubscriptionRepositoryStub,
+    subscriptionRepositoryStub,
+    testPlanEntity
+} from "../__mocks__";
+import { GetUserPlanUseCase, NotFoundError } from "@/layers/application";
+
+const makeSut = (): {
+    sut: GetUserPlanUseCase,
+    subscriptionRepositoryStub: SubscriptionRepositoryStub,
+} => {
+    const sut = new GetUserPlanUseCase(unitOfWorkRepositoryStub);
+
+    return {
+        sut,
+        subscriptionRepositoryStub
+    };
+};
+
+describe("Use case - GetUserPlanUseCase", () => {
+
+    test("Should throw an error if the plan associated with the subscription is not found", async () => {
+        const { sut, subscriptionRepositoryStub } = makeSut();
+        jest.spyOn(subscriptionRepositoryStub, "getActiveSubscriptionByUserId").mockReturnValueOnce(null);
+
+        const result = sut.execute({ userId: "2" });
+
+        await expect(result).rejects.toThrow(NotFoundError);
+    });
+
+    test("Should return the active plan details successfully", async () => {
+        const { sut } = makeSut();
+
+        const result = await sut.execute({ userId: "1" });
+
+        expect(result).toEqual({
+            planId: testPlanEntity.id,
+            planAmount: testPlanEntity.amount,
+            planDescription: testPlanEntity.description,
+            planName: testPlanEntity.name,
+        });
+    });
+});

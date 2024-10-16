87b8fa401d984a8bea36585724e23d04f1ef1d48
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-16 00:07:43 -0300

    feat: add create payment method use case

---
+++ src/layers/application/use-cases/payment-method/create-payment-method/create-payment-method.use-case.ts
@@ -0,0 +1,44 @@
+import {
+	IUnitOfWorkRepository,
+	CreatePaymentMethodDTO,
+	ICreatePaymentMethodUseCase,
+	NotFoundError,
+	ConflictedError,
+	IPayment
+} from "@/layers/application";
+import { PaymentMethodEntity } from "@/layers/domain";
+
+export class CreatePaymentMethodUseCase implements ICreatePaymentMethodUseCase {
+
+	constructor(
+		private readonly unitOfWorkRepository: IUnitOfWorkRepository,
+		private readonly payment: IPayment
+	) { }
+
+	async execute({ userId, name, token }: CreatePaymentMethodDTO): Promise<string> {
+		const userRepository = this.unitOfWorkRepository.getUserRepository();
+		const paymentMethodRepository = this.unitOfWorkRepository.getPaymentMethodRepository();
+		const customerRepository = this.unitOfWorkRepository.getCustomerRepository();
+
+		const userExists = await userRepository.getUserById(userId);
+		if(!userExists) throw new NotFoundError("Esse usuário não existe");
+
+		const paymentMethodByNameExists = await paymentMethodRepository.getPaymentMethodByName(name);
+		if(paymentMethodByNameExists) throw new ConflictedError("Já existe um método de pagamento com esse nome");
+
+		const paymentMethod = new PaymentMethodEntity({
+			userId,
+			name,
+			token
+		}); 
+
+		let paymentMethodCreated: PaymentMethodEntity;
+		await this.unitOfWorkRepository.transaction(async () => {
+			paymentMethodCreated = await paymentMethodRepository.createPaymentMethod(paymentMethod);
+			const customer = await customerRepository.getCustomerByUserId(userId);
+			await this.payment.createPaymentMethod(customer.customerId, token);
+		});
+
+		return paymentMethodCreated.name;
+	}
+}
---
+++ src/layers/application/use-cases/payment-method/create-payment-method/index.ts
@@ -0,0 +1,3 @@
+export * from "./create-payment-method.dtos";
+export * from "./create-payment-method-use-case.interface";
+export * from "./create-payment-method.use-case";

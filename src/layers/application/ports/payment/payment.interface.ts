c0e686ce0cfeca0a33d51cc85b645769f8f0d1f2
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-16 21:43:29 -0300

    refactor: add method in payment interface

--- src/layers/application/ports/payment/payment.interface.ts
+++ src/layers/application/ports/payment/payment.interface.ts
@@ -5,5 +5,6 @@ export interface IPayment {
     updateCustomerEmailByCustomerId(customerId: string, email: string): Promise<void>;
     deleteCustomer(customerId: string): Promise<void>;
     createPaymentMethod(customerId: string, paymentMethodId: string): Promise<void>;
+    deletePaymentMethod(paymentMethodId: string): Promise<void>;
     pay(customerId: string, paymentMethodId: string, amount: number, currency: PaymentCurrencyEnum): Promise<void>;
 }

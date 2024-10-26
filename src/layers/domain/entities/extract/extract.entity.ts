c4fdc6817ce7634610799f335b10739b34f2b7ba
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-25 21:53:45 -0300

    feat: add reference year in extract entity

--- src/layers/domain/entities/extract/extract.entity.ts
+++ src/layers/domain/entities/extract/extract.entity.ts
@@ -1,12 +1,14 @@
 import { AbstractEntity } from "../abstract/abstract.entity";
 import { 
     ExtractUrlValueObject,
-	ExtractReferenceMonthValueObject
+	ExtractReferenceMonthValueObject,
+	ExtractReferenceYearValueObject
 } from "@/layers/domain";
 
 export type ExtractProps = {
     url: string;
     referenceMonth: number;
+    referenceYear: number;
 }
 
 export class ExtractEntity extends AbstractEntity<ExtractProps> {
@@ -17,6 +19,7 @@ export class ExtractEntity extends AbstractEntity<ExtractProps> {
 		const valueObjects = {
 			url: ExtractUrlValueObject.create(props.url),
 			referenceMonth: ExtractReferenceMonthValueObject.create(props.referenceMonth),
+			referenceYear: ExtractReferenceYearValueObject.create(props.referenceYear)
 		};
 
 		this.validate(valueObjects);
@@ -24,6 +27,10 @@ export class ExtractEntity extends AbstractEntity<ExtractProps> {
 
 	public get referenceMonth(): number {
 		return this.props.referenceMonth;
+	}
+
+	public get referenceYear(): number {
+		return this.props.referenceYear;
 	}
 
 	public get url(): string {
--- tests/layers/domain/extract/extract.entity.spec.ts
+++ tests/layers/domain/extract/extract.entity.spec.ts
@@ -2,44 +2,65 @@ import { ExtractEntity, DomainError } from "@/layers/domain";
 
 describe("Entity - Extract", () => {
 
-    test("Should not create ExtractEntity, because URL is invalid", () => {
-        const invalidUrl = "invalid-url";
-        const referenceMonth = 1;
-      
-        const sut = (): ExtractEntity => new ExtractEntity({
-          url: invalidUrl,
-          referenceMonth
-        });
-      
-        expect(sut).toThrow(DomainError);
+  test("Should not create ExtractEntity, because URL is invalid", () => {
+    const invalidUrl = "invalid-url";
+    const referenceMonth = 1;
+    const referenceYear = 2024;
+
+    const sut = (): ExtractEntity => new ExtractEntity({
+      url: invalidUrl,
+      referenceMonth,
+      referenceYear
     });
 
-    test("Should not create ExtractEntity, because reference month is invalid", () => {
-        const url = "https://example.com";
-        const referenceMonth = -1;
+    expect(sut).toThrow(DomainError);
+  });
 
-        const sut = (): ExtractEntity => new ExtractEntity({
-          url,
-          referenceMonth
-        });
+  test("Should not create ExtractEntity, because reference month is invalid", () => {
+    const url = "https://example.com";
+    const referenceMonth = -1;
+    const referenceYear = 2024;
 
-        expect(sut).toThrow(DomainError);
+    const sut = (): ExtractEntity => new ExtractEntity({
+      url,
+      referenceMonth,
+      referenceYear
     });
 
+    expect(sut).toThrow(DomainError);
+  });
 
-    test("Should create ExtractEntity", () => {
-        const url = "https://example.com";
-        const referenceMonth = 1;
-      
-        const sut = new ExtractEntity({
-          url,
-          referenceMonth
-        });
-      
-        expect(sut).toBeInstanceOf(ExtractEntity);
-        expect(sut.id).not.toBeUndefined();
-        expect(sut.referenceMonth).toBe(referenceMonth);
-        expect(sut.url).toBe(url);
-        expect(sut.createdAt).not.toBeUndefined();
+  test("Should not create ExtractEntity, because reference year is invalid", () => {
+    const url = "https://example.com";
+    const referenceMonth = -1;
+    const invalidReferenceYear = 2023;
+
+    const sut = (): ExtractEntity => new ExtractEntity({
+      url,
+      referenceMonth,
+      referenceYear: invalidReferenceYear
     });
+
+    expect(sut).toThrow(DomainError);
+  });
+
+
+  test("Should create ExtractEntity", () => {
+    const url = "https://example.com";
+    const referenceMonth = 1;
+    const referenceYear = 2024;
+
+    const sut = new ExtractEntity({
+      url,
+      referenceMonth,
+      referenceYear
+    });
+
+    expect(sut).toBeInstanceOf(ExtractEntity);
+    expect(sut.id).not.toBeUndefined();
+    expect(sut.referenceMonth).toBe(referenceMonth);
+    expect(sut.referenceYear).toBe(referenceYear);
+    expect(sut.url).toBe(url);
+    expect(sut.createdAt).not.toBeUndefined();
+  });
 });

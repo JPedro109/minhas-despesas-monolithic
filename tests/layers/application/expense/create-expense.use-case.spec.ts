7d6e0dd4295ba6b03b599fd69d65db4152f1cc29
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-30 23:13:23 -0300

    test: add create expense use case unit test

---
+++ tests/layers/application/expense/create-expense.use-case.spec.ts
@@ -0,0 +1,50 @@
+import { DomainError } from "@/layers/domain";
+import { CreateExpenseUseCase } from "@/layers/application";
+import { unitOfWorkRepositoryStub } from "../__mocks__";
+
+const makeSut = (): {
+    sut: CreateExpenseUseCase
+} => {
+    const sut = new CreateExpenseUseCase(unitOfWorkRepositoryStub);
+
+    return {
+        sut
+    };
+};
+
+describe("Use case - CreateExpenseUseCase", () => {
+
+    test("Should not create expense because the user rules are not respected", async () => {
+        const { sut } = makeSut();
+        const userId = "1";
+        const expenseName = "";
+        const expenseValue = -100;
+        const dueDate = new Date("2000-01-01");
+
+        const result = sut.execute({
+            userId,
+            expenseName,
+            expenseValue,
+            dueDate
+        });
+
+        await expect(result).rejects.toThrow(DomainError);
+    });
+
+    test("Should create expense successfully", async () => {
+        const { sut } = makeSut();
+        const userId = "1";
+        const expenseName = "Expense";
+        const expenseValue = 100;    
+        const dueDate = new Date("3000-01-01");
+
+        const result = await sut.execute({
+            userId,
+            expenseName,
+            expenseValue,
+            dueDate
+        });
+
+        expect(result).toBe("1");
+    });
+});

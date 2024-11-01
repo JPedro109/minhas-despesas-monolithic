503bc25d55a761bb9466e19d586bd5ad1f21be6b
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-31 23:35:53 -0300

    test: add update previous month paid expenses to unpaid use case unit test

---
+++ tests/layers/application/expense/update-previous-month-paid-expenses-to-unpaid.use-case.spec.ts
@@ -0,0 +1,32 @@
+import {
+    unitOfWorkRepositoryStub,
+    expenseRepositoryStub,
+    ExpenseRepositoryStub
+} from "../__mocks__";
+import { UpdatePreviousMonthPaidExpensesToUnpaidUseCase } from "@/layers/application";
+
+const makeSut = (): {
+    sut: UpdatePreviousMonthPaidExpensesToUnpaidUseCase,
+    expenseRepositoryStub: ExpenseRepositoryStub
+} => {
+    const sut = new UpdatePreviousMonthPaidExpensesToUnpaidUseCase(unitOfWorkRepositoryStub);
+
+    return {
+        sut,
+        expenseRepositoryStub
+    };
+};
+
+describe("Use case - UpdatePreviousMonthPaidExpensesToUnpaidUseCase", () => {
+
+    test("Should update paid expenses from the previous month to unpaid", async () => {
+        const { sut, expenseRepositoryStub } = makeSut();
+        const previousMonth = new Date().getUTCMonth() - 1;
+        const updatePaidExpensesToUnpaidSpy = jest
+            .spyOn(expenseRepositoryStub, "updatePaidExpensesToUnpaidByDueDateMonth");
+
+        await sut.execute();
+
+        expect(updatePaidExpensesToUnpaidSpy).toHaveBeenCalledWith(previousMonth);
+    });
+});

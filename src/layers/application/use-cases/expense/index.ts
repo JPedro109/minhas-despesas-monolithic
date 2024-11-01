95e7bed17088c3877a9ec038ed4df8aa87754472
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-10-31 23:28:32 -0300

    feat: add update previous month paid expenses to unpaid use case

--- src/layers/application/use-cases/expense/index.ts
+++ src/layers/application/use-cases/expense/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-expense";
 export * from "./get-user-expenses";
 export * from "./update-expense";
+export * from "./update-previous-month-paid-expenses-to-unpaid";
---
+++ src/layers/application/use-cases/expense/update-previous-month-paid-expenses-to-unpaid/update-previous-month-paid-expenses-to-unpaid.use-case.ts
@@ -0,0 +1,13 @@
+import { 
+    IUpdatePreviousMonthPaidExpensesToUnpaidUseCase, 
+    IUnitOfWorkRepository
+} from "@/layers/application";
+
+export class UpdatePreviousMonthPaidExpensesToUnpaidUseCase implements IUpdatePreviousMonthPaidExpensesToUnpaidUseCase {
+    constructor(private readonly unitOfWorkRepository: IUnitOfWorkRepository) { }
+
+    async execute(): Promise<void> {
+        const expenseRepository = this.unitOfWorkRepository.getExpenseRepository();
+        await expenseRepository.updatePaidExpensesToUnpaidByDueDateMonth(new Date().getUTCMonth() - 1);
+    }
+}

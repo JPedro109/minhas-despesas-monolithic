a471c830c23ad997182302cd583dc16277a546b1
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-06 23:11:36 -0300

    test: add get user extracts use case unit test

---
+++ tests/layers/application/extract/get-user-extracts.use-case.spec.ts
@@ -0,0 +1,48 @@
+import {
+    UserRepositoryStub,
+    unitOfWorkRepositoryStub,
+    userRepositoryStub,
+    testExtractEntity
+} from "../__mocks__";
+import { NotFoundError, GetUserExtractsUseCase } from "@/layers/application";
+
+const makeSut = (): {
+    sut: GetUserExtractsUseCase,
+    userRepositoryStub: UserRepositoryStub
+} => {
+    const sut = new GetUserExtractsUseCase(unitOfWorkRepositoryStub);
+    return {
+        sut,
+        userRepositoryStub
+    };
+};
+
+describe("Use case - GetUserExtractsUseCase", () => {
+
+    test("Should not get users extracts because user is not exists", async () => {
+        const { sut, userRepositoryStub } = makeSut();
+        const userId = "2";
+        jest.spyOn(userRepositoryStub, "getUserById").mockResolvedValueOnce(null);
+
+        const result = sut.execute({ userId });
+
+        expect(result).rejects.toThrow(NotFoundError);
+    });
+
+    test("Should update extract url", async () => {
+        const { sut } = makeSut();
+        const userId = "1";
+
+        const result = await sut.execute({ userId });
+
+        expect(result).toEqual(
+            [
+                {
+                    extractId: testExtractEntity.id,
+                    url: testExtractEntity.url,
+                    userId: testExtractEntity.userId
+                }
+            ]
+        );
+    });
+});

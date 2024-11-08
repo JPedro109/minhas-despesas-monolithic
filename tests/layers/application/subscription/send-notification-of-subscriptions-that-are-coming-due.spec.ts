9560a6a1ee50887f4630b5ff4afdee9b1e0cf281
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-08 14:42:53 -0300

    test: add send notification of subscriptions that are coming due use case unit test

---
+++ tests/layers/application/subscription/send-notification-of-subscriptions-that-are-coming-due.spec.ts
@@ -0,0 +1,66 @@
+import { SendNotificationOfSubscriptionThatAreComingDue, MailBodyTypeEnum } from "@/layers/application";
+import {
+    SubscriptionRepositoryStub,
+    MailStub,
+    unitOfWorkRepositoryStub,
+    subscriptionRepositoryStub,
+    mailStub,
+    testUserEntity,
+    testPlanEntity,
+    testSubscriptionEntity
+} from "../__mocks__";
+
+const makeSut = (): {
+    sut: SendNotificationOfSubscriptionThatAreComingDue,
+    subscriptionRepositoryStub: SubscriptionRepositoryStub,
+    mailStub: MailStub
+} => {
+    const sut = new SendNotificationOfSubscriptionThatAreComingDue(
+        unitOfWorkRepositoryStub,
+        mailStub
+    );
+
+    return {
+        sut,
+        subscriptionRepositoryStub,
+        mailStub
+    };
+};
+
+describe("Use case - SendNotificationOfSubscriptionThatAreComingDue", () => {
+
+    test("Should not send any emails if there are no expenses due soon", async () => {
+        const { sut, subscriptionRepositoryStub, mailStub } = makeSut();
+        const sendMailSpy = jest.spyOn(mailStub, "sendMail");
+        jest.spyOn(subscriptionRepositoryStub, "getActiveSubscriptionsByEndDate").mockResolvedValueOnce([]);
+
+        await sut.execute();
+
+        expect(sendMailSpy).not.toHaveBeenCalled();
+    });
+
+    test("Should not send any emails if there are no expenses due soon", async () => {
+        const { sut, mailStub } = makeSut();
+        jest.spyOn(mailStub, "sendMail").mockRejectedValueOnce(new Error());
+
+        const result = sut.execute();
+
+        expect(result).rejects.toThrow(Error);
+    });
+
+    test("Should send notification emails for expenses due soon", async () => {
+        const { sut, mailStub } = makeSut();
+        const sendMailSpy = jest.spyOn(mailStub, "sendMail");
+
+        await sut.execute();
+
+        expect(sendMailSpy).toHaveBeenCalledWith(
+            testUserEntity.email,
+            MailBodyTypeEnum.NotifySubscriptionThatIsDueBody,
+            { 
+                planName: testPlanEntity.name, 
+                value: testSubscriptionEntity().amount 
+            } 
+        );
+    });
+});

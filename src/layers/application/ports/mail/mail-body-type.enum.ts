2fb62dfd923ccd65c712ad6f573b0c5ae099c3f1
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-08 14:26:34 -0300

    refactor: add notify expense that is due body of mail body type enum

--- src/layers/application/ports/mail/mail-body-type.enum.ts
+++ src/layers/application/ports/mail/mail-body-type.enum.ts
@@ -2,5 +2,6 @@ export enum MailBodyTypeEnum {
 	VerifyUserEmailBody = "verify_user_email_body",
 	UpdateUserEmailBody = "update_user_email_body",
 	RecoveryUserPasswordBody = "recovery_user_password_body",
-	NotifyExpenseThatIsDueBody = "notify_expense_that_is_due_body"
+	NotifyExpenseThatIsDueBody = "notify_expense_that_is_due_body",
+	NotifySubscriptionThatIsDueBody = "notify_subscription_that_is_due_body"
 }

6189b6be764d366f6f3e61e6130aa1c55355130c
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-11 23:49:23 -0300

    test: add execute charge to expired subscriptions unit test

---
+++ tests/layers/application/subscription/execute-charge-to-expired-subscriptions.use-case.spec.ts
@@ -0,0 +1,56 @@
+import { ExecuteChargeToExpiredSubscriptions, PaymentCurrencyEnum } from "@/layers/application";
+import {
+    PaymentStub,
+    SubscriptionRepositoryStub,
+    unitOfWorkRepositoryStub,
+    paymentStub,
+    testSubscriptionEntity,
+    testPaymentMethodEntity,
+    testCustomerEntity,
+    subscriptionRepositoryStub,
+} from "../__mocks__";
+
+const makeSut = (): {
+    sut: ExecuteChargeToExpiredSubscriptions,
+    paymentStub: PaymentStub,
+    subscriptionRepositoryStub: SubscriptionRepositoryStub
+} => {
+    const sut = new ExecuteChargeToExpiredSubscriptions(
+        unitOfWorkRepositoryStub,
+        paymentStub
+    );
+
+    return {
+        sut,
+        paymentStub,
+        subscriptionRepositoryStub
+    };
+};
+
+describe("Use case - ExecuteChargeToExpiredSubscriptions", () => {
+
+    test("Should not execute charge if there are no subscriptions due soon", async () => {
+        const { sut, subscriptionRepositoryStub } = makeSut();
+        const paySpy = jest.spyOn(paymentStub, "pay");
+        jest.spyOn(subscriptionRepositoryStub, "getActiveSubscriptionsByEndDate").mockResolvedValueOnce([]);
+
+        await sut.execute();
+
+        expect(paySpy).not.toHaveBeenCalled();
+    });
+
+
+    test("Should execute charge", async () => {
+        const { sut, paymentStub } = makeSut();
+        const paySpy = jest.spyOn(paymentStub, "pay");
+
+        await sut.execute();
+
+        expect(paySpy).toHaveBeenCalledWith(
+            testCustomerEntity.customerId, 
+            testPaymentMethodEntity.token, 
+            testSubscriptionEntity().amount, 
+            PaymentCurrencyEnum.BRL
+        );
+    });
+});

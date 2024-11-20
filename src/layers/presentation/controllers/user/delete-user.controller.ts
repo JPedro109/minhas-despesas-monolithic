587d4b88452963adaa4cdedd385f000e8aa2362c
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-20 17:20:58 -0300

    feat: add delete user controller

---
+++ src/layers/presentation/controllers/user/delete-user.controller.ts
@@ -0,0 +1,44 @@
+import { IDeleteUserUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpRequest, HttpResponse, HttpHelper } from "@/layers/presentation";
+
+export class DeleteUserController extends AbstractController {
+
+    constructor(
+        private readonly useCase: IDeleteUserUseCase,
+        log: ILog
+    ) { 
+        super(
+            log,
+            {
+                id: {
+                    type: "string",
+                    optional: false
+                },
+                password: {
+                    type: "string",
+                    optional: false
+                },
+                passwordConfirm: {
+                    type: "string",
+                    optional: false
+                }
+            },
+            "DeleteUser"
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const { password, passwordConfirm } = request.data;
+        const body = {
+            id: request.userId,
+            password, 
+            passwordConfirm
+        };
+
+        this.validateRequestSchema(body);
+
+        await this.useCase.execute(body);
+
+        return HttpHelper.noBody();
+    }
+}

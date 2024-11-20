14a4f9cad5f0d71d525b50664ec1e672e444f3d9
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-20 18:11:02 -0300

    feat: add send user password recovery link controller

--- src/layers/presentation/controllers/user/index.ts
+++ src/layers/presentation/controllers/user/index.ts
@@ -3,3 +3,4 @@ export * from "./delete-user.controller";
 export * from "./recover-user-password.controller";
 export * from "./refresh-user-token.controller";
 export * from "./send-user-email-update-link.controller";
+export * from "./send-user-password-recovery-link.controller";
---
+++ src/layers/presentation/controllers/user/send-user-password-recovery-link.controller.ts
@@ -0,0 +1,33 @@
+import { ISendUserPasswordRecoveryLinkUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpRequest, HttpResponse, HttpHelper } from "@/layers/presentation";
+
+export class SendUserPasswordRecoveryLinkController extends AbstractController {
+
+    constructor(
+        private readonly useCase: ISendUserPasswordRecoveryLinkUseCase,
+        log: ILog
+    ) { 
+        super(
+            log,
+            {
+                email: {
+                    type: "string",
+                    optional: false
+                }
+            },
+            "SendUserPasswordRecoveryLink"
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const { email } = request.data;
+
+        const body = { email };
+
+        this.validateRequestSchema(body);
+
+        await this.useCase.execute(body);
+
+        return HttpHelper.noBody();
+    }
+}

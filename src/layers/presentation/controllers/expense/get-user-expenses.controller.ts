b1b8d3fe00185f5cf8eafc0e61e5846c4eefb820
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 19:35:06 -0300

    feat: add get user expenses controller

---
+++ src/layers/presentation/controllers/expense/get-user-expenses.controller.ts
@@ -0,0 +1,33 @@
+import { IGetUserExpensesUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpResponse, HttpHelper, HttpRequest } from "@/layers/presentation";
+
+export class GetUserExpensesController extends AbstractController {
+
+    constructor(
+        private readonly useCase: IGetUserExpensesUseCase,
+        log: ILog
+    ) {
+        super(
+            log,
+            "GetUserExpenses",
+            {
+                userId: {
+                    type: "string",
+                    optional: false
+                }
+            }
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const body = {
+            userId: request.userId
+        };
+
+        this.validateRequestSchema(body);
+
+        const response = await this.useCase.execute(body);
+
+        return HttpHelper.ok(response);
+    }
+}
--- src/layers/presentation/controllers/expense/index.ts
+++ src/layers/presentation/controllers/expense/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-expense.controller";
 export * from "./delete-expense.controller";
 export * from "./expense-undo-payment.controller";
+export * from "./get-user-expenses.controller";

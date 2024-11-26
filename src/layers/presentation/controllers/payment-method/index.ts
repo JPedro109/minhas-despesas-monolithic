5643609cd8cb36b1ac4e9efe29ef3f7f80697361
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 00:10:19 -0300

    feat: add update payment method token controller

--- src/layers/presentation/controllers/payment-method/index.ts
+++ src/layers/presentation/controllers/payment-method/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-payment-method.controller";
 export * from "./delete-payment-method.controller";
 export * from "./update-payment-method-name.controller";
+export * from "./update-payment-method-token.controller";
---
+++ src/layers/presentation/controllers/payment-method/update-payment-method-token.controller.ts
@@ -0,0 +1,39 @@
+import { IUpdatePaymentMethodTokenUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpResponse, HttpHelper, HttpRequest } from "@/layers/presentation";
+
+export class UpdatePaymentMethodTokenController extends AbstractController {
+
+    constructor(
+        private readonly useCase: IUpdatePaymentMethodTokenUseCase,
+        log: ILog
+    ) { 
+        super(
+            log,
+            "UpdatePaymentMethodToken",
+            {
+                id: {
+                    type: "string",
+                    optional: false
+                },
+                token: {
+                    type: "string",
+                    optional: false
+                }
+            }
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const { id, token } = request.data;
+        const body = { 
+            id,
+            token
+        };
+
+        this.validateRequestSchema(body);
+
+        await this.useCase.execute(body);
+
+        return HttpHelper.noBody();
+    }
+}

4fb714d938a2523424d9c18d0b95c08f5cd7aa30
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 19:49:26 -0300

    test: add send notification of expenses that are coming due controller unit test

---
+++ tests/layers/presentation/controllers/expense/send-notification-of-expenses-that-are-coming-due.controller.spec.ts
@@ -0,0 +1,34 @@
+import { ISendNotificationOfExpensesThatAreComingDueUseCase } from "@/layers/application";
+import { SendNotificationOfExpensesThatAreComingDueController } from "@/layers/presentation";
+import { logStubFactory } from "../__mocks__";
+
+const makeSut = (): {
+    sut: SendNotificationOfExpensesThatAreComingDueController,
+    mockSendNotificationOfExpensesThatAreComingDueUseCase: jest.Mocked<ISendNotificationOfExpensesThatAreComingDueUseCase>
+} => {
+    const mockSendNotificationOfExpensesThatAreComingDueUseCase: jest.Mocked<ISendNotificationOfExpensesThatAreComingDueUseCase> = {
+        execute: jest.fn()
+    };
+    const logStub = logStubFactory();
+
+    const sut = new SendNotificationOfExpensesThatAreComingDueController(
+        mockSendNotificationOfExpensesThatAreComingDueUseCase,
+        logStub
+    );
+
+    return {
+        sut,
+        mockSendNotificationOfExpensesThatAreComingDueUseCase
+    };
+};
+
+describe("Controller - SendNotificationOfExpensesThatAreComingDueController", () => {
+
+    test("Should send notification of expenses that are coming due payment", async () => {
+        const { sut } = makeSut();
+
+        const result = await sut.http({});
+
+        expect(result.statusCode).toBe(204);
+    });
+});

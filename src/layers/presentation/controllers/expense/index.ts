f5bffb1293f8c85ec19442273851dce52260fdf6
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 19:52:32 -0300

    feat: add update expense controller

--- src/layers/presentation/controllers/expense/index.ts
+++ src/layers/presentation/controllers/expense/index.ts
@@ -3,3 +3,4 @@ export * from "./delete-expense.controller";
 export * from "./expense-undo-payment.controller";
 export * from "./get-user-expenses.controller";
 export * from "./send-notification-of-expenses-that-are-coming-due.controller";
+export * from "./update-expense.controller";
---
+++ src/layers/presentation/controllers/expense/update-expense.controller.ts
@@ -0,0 +1,59 @@
+import { IUpdateExpenseUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpResponse, HttpHelper, HttpRequest } from "@/layers/presentation";
+
+export class UpdateExpenseController extends AbstractController {
+
+    constructor(
+        private readonly useCase: IUpdateExpenseUseCase,
+        log: ILog
+    ) {
+        super(
+            log,
+            "UpdateExpense",
+            {
+                userId: {
+                    type: "string",
+                    optional: false
+                },
+                id: {
+                    type: "string",
+                    optional: false
+                },
+                expenseName: {
+                    type: "string",
+                    optional: false
+                },
+                expenseValue: {
+                    type: "number",
+                    optional: false
+                },
+                dueDate: {
+                    type: "string",
+                    optional: false
+                }
+            }
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const {
+            id,
+            expenseName,
+            expenseValue,
+            dueDate
+        } = request.data;
+        const body = {
+            id,
+            userId: request.userId,
+            expenseName,
+            expenseValue,
+            dueDate
+        };
+
+        this.validateRequestSchema(body);
+
+        await this.useCase.execute(body);
+
+        return HttpHelper.noBody();
+    }
+}

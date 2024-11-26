a6a51d657884afc04e276ee4b5ffb9d7ff10f47c
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 19:45:41 -0300

    feat: add send notification of expenses that are coming due controller

--- src/layers/presentation/controllers/expense/index.ts
+++ src/layers/presentation/controllers/expense/index.ts
@@ -2,3 +2,4 @@ export * from "./create-expense.controller";
 export * from "./delete-expense.controller";
 export * from "./expense-undo-payment.controller";
 export * from "./get-user-expenses.controller";
+export * from "./send-notification-of-expenses-that-are-coming-due.controller";
---
+++ src/layers/presentation/controllers/expense/send-notification-of-expenses-that-are-coming-due.controller.ts
@@ -0,0 +1,21 @@
+import { ISendNotificationOfExpensesThatAreComingDueUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpResponse, HttpHelper } from "@/layers/presentation";
+
+export class SendNotificationOfExpensesThatAreComingDueController extends AbstractController {
+
+    constructor(
+        private readonly useCase: ISendNotificationOfExpensesThatAreComingDueUseCase,
+        log: ILog
+    ) {
+        super(
+            log,
+            "SendNotificationOfExpensesThatAreComingDue"
+        );
+    }
+
+    protected async handler(): Promise<HttpResponse> {
+        await this.useCase.execute();
+
+        return HttpHelper.noBody();
+    }
+}

f3d9a8367fafc20e83dc73581972bb06ebe33568
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 19:28:54 -0300

    feat: add expense undo payment controller

---
+++ src/layers/presentation/controllers/expense/expense-undo-payment.controller.ts
@@ -0,0 +1,34 @@
+import { IExpenseUndoPaymentUseCase, ILog } from "@/layers/application";
+import { AbstractController, HttpResponse, HttpHelper, HttpRequest } from "@/layers/presentation";
+
+export class ExpenseUndoPaymentController extends AbstractController {
+
+    constructor(
+        private readonly useCase: IExpenseUndoPaymentUseCase,
+        log: ILog
+    ) {
+        super(
+            log,
+            "ExpenseUndoPayment",
+            {
+                id: {
+                    type: "string",
+                    optional: false
+                }
+            }
+        );
+    }
+
+    protected async handler(request: HttpRequest): Promise<HttpResponse> {
+        const { id } = request.data;
+        const body = {
+            id
+        };
+
+        this.validateRequestSchema(body);
+
+        await this.useCase.execute(body);
+
+        return HttpHelper.noBody();
+    }
+}
--- src/layers/presentation/controllers/expense/index.ts
+++ src/layers/presentation/controllers/expense/index.ts
@@ -1,2 +1,3 @@
 export * from "./create-expense.controller";
 export * from "./delete-expense.controller";
+export * from "./expense-undo-payment.controller";

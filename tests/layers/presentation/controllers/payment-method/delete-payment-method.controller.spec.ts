70ad70ab900f2f866ca63d362d613851fd2c4f28
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 00:02:21 -0300

    test: add delete payment method controller unit test

---
+++ tests/layers/presentation/controllers/payment-method/delete-payment-method.controller.spec.ts
@@ -0,0 +1,52 @@
+import { IDeletePaymentMethodUseCase } from "@/layers/application";
+import { DeletePaymentMethodController } from "@/layers/presentation";
+import { logStubFactory } from "../__mocks__";
+
+const makeSut = (): {
+    sut: DeletePaymentMethodController,
+    mockExecuteChargeToExpiredSubscriptionUseCase: jest.Mocked<IDeletePaymentMethodUseCase>
+} => {
+    const mockExecuteChargeToExpiredSubscriptionUseCase: jest.Mocked<IDeletePaymentMethodUseCase> = {
+        execute: jest.fn()
+    };
+    const logStub = logStubFactory();
+
+    const sut = new DeletePaymentMethodController(
+        mockExecuteChargeToExpiredSubscriptionUseCase, 
+        logStub
+    );
+
+    return {
+        sut,
+        mockExecuteChargeToExpiredSubscriptionUseCase
+    };
+};
+
+describe("Controller - DeletePaymentMethodController", () => {
+
+    test("Should not delete payment method because schema is invalid", async () => {
+        const { sut } = makeSut();
+        const id = "";
+
+        const result = await sut.http({
+            data: {
+                id
+            }
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    test("Should delete payment method", async () => {
+        const { sut } = makeSut();
+        const id = "1";
+
+        const result = await sut.http({
+            data: {
+                id
+            }
+        });
+
+        expect(result.statusCode).toBe(204);
+    });
+});

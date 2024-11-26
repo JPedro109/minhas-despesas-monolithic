5ec9430f3aea834cfbb3c86476a2a2ea69e258bf
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 01:22:36 -0300

    test: add delete expense controller unit test

---
+++ tests/layers/presentation/controllers/expense/delete-expense.controller.spec.ts
@@ -0,0 +1,56 @@
+import { IDeleteExpenseUseCase } from "@/layers/application";
+import { DeleteExpenseController } from "@/layers/presentation";
+import { logStubFactory } from "../__mocks__";
+
+const makeSut = (): {
+    sut: DeleteExpenseController,
+    mockDeleteExpenseUseCase: jest.Mocked<IDeleteExpenseUseCase>
+} => {
+    const mockDeleteExpenseUseCase: jest.Mocked<IDeleteExpenseUseCase> = {
+        execute: jest.fn().mockResolvedValue("1")
+    };
+    const logStub = logStubFactory();
+
+    const sut = new DeleteExpenseController(
+        mockDeleteExpenseUseCase,
+        logStub
+    );
+
+    return {
+        sut,
+        mockDeleteExpenseUseCase
+    };
+};
+
+describe("Controller - DeleteExpenseController", () => {
+
+    test("Should not delete expense because schema is invalid", async () => {
+        const { sut } = makeSut();
+        const id = "";
+        const deleteExpensePaymentHistory = undefined;
+        
+        const result = await sut.http({
+            data: {
+                id,
+                deleteExpensePaymentHistory
+            }
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    test("Should delete expense", async () => {
+        const { sut } = makeSut();
+        const id = "1";
+        const deleteExpensePaymentHistory = true;
+
+        const result = await sut.http({
+            data: {
+                id,
+                deleteExpensePaymentHistory
+            }
+        });
+
+        expect(result.statusCode).toBe(204);
+    });
+});

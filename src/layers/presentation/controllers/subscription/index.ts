45b565f706b5692b0c49cdf2d5621f1a0eeb814d
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-25 23:13:46 -0300

    feat: add send notification of subscriptions that are coming due controller

--- src/layers/presentation/controllers/subscription/index.ts
+++ src/layers/presentation/controllers/subscription/index.ts
@@ -1,3 +1,4 @@
 export * from "./execute-charge-to-expired-subscriptions.controller";
 export * from "./set-free-plan-for-non-renewable-subscriptions.controller";
 export * from "./manage-subscription-renewal.controller";
+export * from "./send-notification-of-subscriptions-that-are-coming-due.controller";

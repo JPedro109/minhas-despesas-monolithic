9ec40b5e5386b0e87256ade7b3fa082636136f3c
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 01:08:29 -0300

    test: add create expense controller unit test

---
+++ tests/layers/presentation/controllers/expense/create-expense.controller.spec.ts
@@ -0,0 +1,64 @@
+import { ICreateExpenseUseCase } from "@/layers/application";
+import { CreateExpenseController } from "@/layers/presentation";
+import { logStubFactory } from "../__mocks__";
+
+const makeSut = (): {
+    sut: CreateExpenseController,
+    mockCreateExpenseUseCase: jest.Mocked<ICreateExpenseUseCase>
+} => {
+    const mockCreateExpenseUseCase: jest.Mocked<ICreateExpenseUseCase> = {
+        execute: jest.fn().mockResolvedValue("1")
+    };
+    const logStub = logStubFactory();
+
+    const sut = new CreateExpenseController(
+        mockCreateExpenseUseCase, 
+        logStub
+    );
+
+    return {
+        sut,
+        mockCreateExpenseUseCase
+    };
+};
+
+describe("Controller - CreateExpenseController", () => {
+
+    test("Should not create expense because schema is invalid", async () => {
+        const { sut } = makeSut();
+        const userId = "";
+        const expenseName ="";
+        const expenseValue = null;
+        const dueDate = "";
+
+        const result = await sut.http({
+            data: {
+                expenseName,
+                expenseValue,
+                dueDate
+            },
+            userId
+        });
+
+        expect(result.statusCode).toBe(400);
+    });
+
+    test("Should create expense", async () => {
+        const { sut } = makeSut();
+        const userId = "1";
+        const expenseName = "Name";
+        const expenseValue = 100;
+        const dueDate = "3000-01-01";
+
+        const result = await sut.http({
+            data: {
+                expenseName,
+                expenseValue,
+                dueDate
+            },
+            userId
+        });
+
+        expect(result.statusCode).toBe(201);
+    });
+});

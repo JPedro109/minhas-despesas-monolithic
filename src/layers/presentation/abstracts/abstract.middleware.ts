38bda1996fee3e8b291cc12077942472143aed48
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-26 23:07:26 -0300

    feat: add abstract middleware

---
+++ src/layers/presentation/abstracts/abstract.middleware.ts
@@ -0,0 +1,23 @@
+import { ForbiddenError, UnauthorizedError } from "@/layers/application";
+import { HttpHelper, HttpRequest, HttpResponse, IHttp } from "@/layers/presentation";
+
+export abstract class AbstractMiddleware implements IHttp {
+
+    public async http(request: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { statusCode, response } = await this.handler(request);
+
+            return { statusCode, response };
+        } catch (e) {
+            return this.setErrorStatusCode(e);
+        }
+    }
+
+    protected abstract handler(request: HttpRequest): Promise<HttpResponse>;
+
+    private setErrorStatusCode(e: Error): HttpResponse {
+        if (e instanceof UnauthorizedError) return HttpHelper.unauthorized(e);
+        if (e instanceof ForbiddenError) return HttpHelper.forbidden(e);
+        return HttpHelper.serverError();
+    }
+}

828611fdbf8f08c200c8160f40a4c20673f5779d
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-28 00:09:00 -0300

    refactor: remove environment variables validation

--- src/shared/environment-variables.ts
+++ src/shared/environment-variables.ts
@@ -7,18 +7,3 @@ type EnvironmentVariables = {
 export const environmentVariables: EnvironmentVariables = {
     appUrl: process.env.APP_URL
 };
-
-((environmentVariables: EnvironmentVariables): void => {
-    const nullVariables: string[] = [];
-
-    for (const variable in environmentVariables) {
-        const value = environmentVariables[variable];
-
-        if (value === null) nullVariables.push(variable);
-    }
-
-    if (nullVariables.length > 0) {
-        const messageError = `The environment variables ${nullVariables.join(", ")} are null`;
-        throw new Error(messageError);
-    }
-})(environmentVariables);

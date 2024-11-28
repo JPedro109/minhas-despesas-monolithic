1c20c686a4637a1bc84494f7ee133eeffce464c2
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-11-27 23:52:56 -0300

    test: add get user subscription use case unit test

---
+++ tests/layers/application/subscription/get-user-subscription.use-case.spec.ts
@@ -0,0 +1,66 @@
+import { NotFoundError, GetUserSubscriptionUseCase } from "@/layers/application";
+import {
+    SubscriptionRepositoryStub,
+    UserRepositoryStub,
+    testSubscriptionEntityWithPlanDiamond,
+    unitOfWorkRepositoryStubFactory
+} from "../__mocks__";
+
+const makeSut = (): {
+    sut: GetUserSubscriptionUseCase,
+    userRepositoryStub: UserRepositoryStub,
+    subscriptionRepositoryStub: SubscriptionRepositoryStub
+} => {
+    const unitOfWorkRepositoryStub = unitOfWorkRepositoryStubFactory();
+    const sut = new GetUserSubscriptionUseCase(unitOfWorkRepositoryStub);
+
+    return {
+        sut,
+        userRepositoryStub: unitOfWorkRepositoryStub.getUserRepository(),
+        subscriptionRepositoryStub: unitOfWorkRepositoryStub.getSubscriptionRepository()
+    };
+};
+
+describe("Use case - GetUserSubscriptionUseCase", () => {
+
+    test("Should not get user subscription because user does not exist", async () => {
+        const { sut, userRepositoryStub } = makeSut();
+        const userId = "2";
+        jest.spyOn(userRepositoryStub, "getUserById").mockReturnValueOnce(null);
+
+        const result = sut.execute({ userId });
+
+        await expect(result).rejects.toThrow(NotFoundError);
+    });
+
+    test("Should get user subscription successfully", async () => {
+        const { sut, subscriptionRepositoryStub } = makeSut();
+        const userId = "1";
+        jest.spyOn(subscriptionRepositoryStub, "getActiveSubscriptionByUserId").mockResolvedValue(testSubscriptionEntityWithPlanDiamond());
+
+        const result = await sut.execute({ userId });
+
+        expect(result).toEqual(
+            {
+                userId: testSubscriptionEntityWithPlanDiamond().userId,
+                amount: testSubscriptionEntityWithPlanDiamond().amount,
+                active: testSubscriptionEntityWithPlanDiamond().active,
+                renewable: testSubscriptionEntityWithPlanDiamond().renewable,
+                startDate: testSubscriptionEntityWithPlanDiamond().startDate,
+                endDate: testSubscriptionEntityWithPlanDiamond().endDate,
+                plan: {
+                    name: testSubscriptionEntityWithPlanDiamond().plan.name,
+                    amount: testSubscriptionEntityWithPlanDiamond().plan.amount,
+                    description: testSubscriptionEntityWithPlanDiamond().plan.description,
+                    durationInDays: testSubscriptionEntityWithPlanDiamond().plan.durationInDays,
+                    actions: testSubscriptionEntityWithPlanDiamond().plan.actions.map(action => ({
+                        id: action.id,
+                        name: action.name,
+                        description: action.description,
+                        totalOperations: action.totalOperations
+                    }))
+                }
+            }
+        );
+    });
+});

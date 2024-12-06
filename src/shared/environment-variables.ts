4fb72418c74c967596a7d0a191be2c9f81334caa
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-05 21:56:28 -0300

    chore: add environment variables

--- src/shared/environment-variables.ts
+++ src/shared/environment-variables.ts
@@ -1,9 +1,15 @@
 import "dotenv/config";
 
 type EnvironmentVariables = {
-    appUrl: string
+    appUrl: string;
+    jwtKey: string;
+    basicAuthenticationUser: string;
+    basicAuthenticationPassword: string;
 }
 
 export const environmentVariables: EnvironmentVariables = {
-    appUrl: process.env.APP_URL
+    appUrl: process.env.APP_URL,
+    jwtKey: process.env.JWT_KEY,
+    basicAuthenticationUser: process.env.BASIC_AUTHENTICATION_USER,
+    basicAuthenticationPassword: process.env.BASIC_AUTHENTICATION_PASSWORD
 };

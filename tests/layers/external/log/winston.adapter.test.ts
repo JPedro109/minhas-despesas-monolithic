9188d00afcb51e01ee4226e798c940a589286dc1
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-11 00:27:41 -0300

    test: add winston adapter test

---
+++ tests/layers/external/log/winston.adapter.test.ts
@@ -0,0 +1,53 @@
+import { WinstonAdapter } from "@/layers/external";
+
+describe("External - WinstonAdapter", () => {
+    
+	test("Should return true | trace", () => {
+		const message = "{\"name\":\"test\"}";
+		const trace = "0000000000";
+		const sut = new WinstonAdapter();
+		jest.spyOn(sut, "trace");
+
+		const result = sut.trace(message, trace);
+
+		expect(result).toBeTruthy();
+		expect(sut.trace).toHaveBeenCalled();
+		expect(sut.trace).toHaveBeenCalledWith(message, trace);
+	});
+
+	test("Should return true | info", () => {
+		const message = "{\"name\":\"test\"}";
+		const sut = new WinstonAdapter();
+		jest.spyOn(sut, "info");
+
+		const result = sut.info(message);
+
+		expect(result).toBeTruthy();
+		expect(sut.info).toHaveBeenCalled();
+		expect(sut.info).toHaveBeenCalledWith(message);
+	});
+
+	test("Should return true | warning", () => {
+		const message = "{\"name\":\"test\"}";
+		const sut = new WinstonAdapter();
+		jest.spyOn(sut, "warning");
+
+		const result = sut.warning(message);
+
+		expect(result).toBeTruthy();
+		expect(sut.warning).toHaveBeenCalled();
+		expect(sut.warning).toHaveBeenCalledWith(message);
+	});
+
+	test("Should return true | error", () => {
+		const message = "{\"name\":\"test\"}";
+		const sut = new WinstonAdapter();
+		jest.spyOn(sut, "error");
+
+		const result = sut.error(message, new Error());
+
+		expect(result).toBeTruthy();
+		expect(sut.error).toHaveBeenCalled();
+		expect(sut.error).toHaveBeenCalledWith(message, new Error());
+	});
+});

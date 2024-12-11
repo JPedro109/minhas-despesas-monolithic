609e5d474a6023c8c3e46ff537dbbf2ad98f0e62
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-10 23:43:58 -0300

    chore: add environment variables

--- src/shared/environment-variables.ts
+++ src/shared/environment-variables.ts
@@ -5,11 +5,21 @@ type EnvironmentVariables = {
     jwtKey: string;
     basicAuthenticationUser: string;
     basicAuthenticationPassword: string;
+    nodeEnv: string;
+    awsAccessKeyId: string;
+    secretAccessKey: string;
+    awsRegion: string;
+    awsBucketName: string;
 }
 
 export const environmentVariables: EnvironmentVariables = {
     appUrl: process.env.APP_URL,
     jwtKey: process.env.JWT_KEY,
     basicAuthenticationUser: process.env.BASIC_AUTHENTICATION_USER,
-    basicAuthenticationPassword: process.env.BASIC_AUTHENTICATION_PASSWORD
+    basicAuthenticationPassword: process.env.BASIC_AUTHENTICATION_PASSWORD,
+    nodeEnv: process.env.NODE_ENV,
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    awsRegion: process.env.AWS_REGION,
+    awsBucketName: process.env.AWS_BUCKET_NAME
 };

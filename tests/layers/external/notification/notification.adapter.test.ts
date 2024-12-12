e3460afc9131f29c704589d9285cb85b3e38234c
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-11 23:19:56 -0300

    test: add notification adapter test

---
+++ tests/layers/external/notification/notification.adapter.test.ts
@@ -0,0 +1,17 @@
+import { NotificationAdapter, SQSAdapter } from "@/layers/external";
+
+describe("External - NotificationAdapter", () => {
+	test("Should send email | sendEmail", async () => {
+		const email = "email@test.com";
+		const type = "Test";
+		const props = { name: "Test" };
+		const queueAdapter = new SQSAdapter();
+		const sut = new NotificationAdapter(queueAdapter);
+		jest.spyOn(sut, "sendMail");
+        
+		await sut.sendMail(email, type, props);
+
+		expect(sut.sendMail).toHaveBeenCalled();
+		expect(sut.sendMail).toHaveBeenCalledWith(email, type, props);
+	});
+});

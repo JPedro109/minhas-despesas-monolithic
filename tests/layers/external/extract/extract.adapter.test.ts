5821634e46bead3cfbbd7087a5b1ffa28046736f
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-20 00:55:07 -0300

    test: add extract adapter test

---
+++ tests/layers/external/extract/extract.adapter.test.ts
@@ -0,0 +1,32 @@
+import { ExtractAdapter } from "@/layers/external";
+import { ExpenseExtractProps } from "@/layers/application";
+
+describe("External - ExtractAdapter", () => {
+    test("Should generate a valid buffer for the expense statement", async () => {
+        const sut = new ExtractAdapter();
+        const data: ExpenseExtractProps = {
+            referenceMonth: 12,
+            referenceYear: 2024,
+            data: [
+                {
+                    expenseId: "1",
+                    expenseName: "Energia Elétrica",
+                    expenseValue: 250.75,
+                    dueDate: new Date("2024-12-10"),
+                    paidDate: new Date("2024-12-05"),
+                },
+                {
+                    expenseId: "2",
+                    expenseName: "Água",
+                    expenseValue: 120.0,
+                    dueDate: new Date("2024-12-12"),
+                    paidDate: new Date("2024-12-06"),
+                },
+            ],
+        };
+
+        const pdfBuffer = await sut.generateExpensesExtract(data);
+
+        expect(pdfBuffer).toBeInstanceOf(Buffer);
+    });
+});

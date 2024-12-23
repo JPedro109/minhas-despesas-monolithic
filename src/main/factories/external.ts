2f86b8b83a08754e58e0f412a65f50067bf9621a
João Pedro <joaopedromoraesnsilva@gmail.com>
2024-12-23 01:08:29 -0300

    feat: add factories

---
+++ src/main/factories/external.ts
@@ -0,0 +1,59 @@
+import {
+    BcryptJSAdapter,
+    ExtractAdapter,
+    GenerationAdapter,
+    WinstonAdapter,
+    NotificationAdapter,
+    StripeAdapter,
+    SQSAdapter,
+    SecurityAdapter,
+    DatabaseSQLHelper,
+    PrismaCustomerRepositoryAdapter,
+    PrismaExpenseRepositoryAdapter,
+    PrismaExtractRepositoryAdapter,
+    PrismaPaymentHistoryRepositoryAdapter,
+    PrismaPaymentMethodRepositoryAdapter ,
+    PrismaSubscriptionRepositoryAdapter,
+    PrismaUserConsentRepositoryAdapter,
+    PrismaUserRepositoryAdapter,
+    PrismaUserVerificationCodeRepositoryAdapter,
+    PrismaPlanRepositoryAdapter,
+    PrismaUnitOfWorkRepositoryAdapter,
+    S3BucketAdapter
+} from "@/layers/external";
+
+export const bcryptJSAdapter = new BcryptJSAdapter();
+export const extractAdapter = new ExtractAdapter();
+export const generationAdapter = new GenerationAdapter();
+export const winstonAdapter = new WinstonAdapter();
+export const sqsAdapter = new SQSAdapter();
+export const s3BucketAdapter = new S3BucketAdapter();
+export const notificationAdapter = new NotificationAdapter(sqsAdapter);
+export const stripeAdapter = new StripeAdapter();
+export const securityAdapter = new SecurityAdapter();
+
+const databaseSQLHelper = new DatabaseSQLHelper();
+
+export const prismaCustomerRepositoryAdapter = new PrismaCustomerRepositoryAdapter(databaseSQLHelper);
+export const prismaExpenseRepositoryAdapter = new PrismaExpenseRepositoryAdapter(databaseSQLHelper);
+export const prismaExtractRepositoryAdapter = new PrismaExtractRepositoryAdapter(databaseSQLHelper);
+export const prismaPaymentHistoryRepositoryAdapter = new PrismaPaymentHistoryRepositoryAdapter(databaseSQLHelper);
+export const prismaPaymentMethodRepositoryAdapter = new PrismaPaymentMethodRepositoryAdapter(databaseSQLHelper);
+export const prismaUserRepositoryAdapter = new PrismaUserRepositoryAdapter(databaseSQLHelper);
+export const prismaUserConsentRepositoryAdapter = new PrismaUserConsentRepositoryAdapter(databaseSQLHelper);
+export const prismaUserVerificationCodeRepositoryAdapter = new PrismaUserVerificationCodeRepositoryAdapter(databaseSQLHelper);
+export const prismaSubscriptionRepositoryAdapter = new PrismaSubscriptionRepositoryAdapter(databaseSQLHelper);
+export const prismaPlanRepositoryAdapter = new PrismaPlanRepositoryAdapter(databaseSQLHelper);
+export const makePrismaUnitOfWorkRepositoryAdapter = (): PrismaUnitOfWorkRepositoryAdapter => new PrismaUnitOfWorkRepositoryAdapter(
+    databaseSQLHelper,
+    prismaUserRepositoryAdapter,
+    prismaUserConsentRepositoryAdapter,
+    prismaUserVerificationCodeRepositoryAdapter,
+    prismaCustomerRepositoryAdapter,
+    prismaPlanRepositoryAdapter,
+    prismaSubscriptionRepositoryAdapter,
+    prismaExpenseRepositoryAdapter,
+    prismaPaymentMethodRepositoryAdapter,
+    prismaPaymentHistoryRepositoryAdapter,
+    prismaExtractRepositoryAdapter
+);
---
+++ src/main/factories/presentation.ts
@@ -0,0 +1,203 @@
+import {
+    CreateExpenseController,
+    DeleteExpenseController,
+    UpdateExpenseController,
+    GetUserExtractsController,
+    GetUserExpensesController,
+    ExpenseUndoPaymentController,
+    UpdatePreviousMonthPaidExpensesToUnpaidController,
+    SendNotificationOfExpensesThatAreComingDueController,
+    DeleteExpiredExtractsController,
+    PayExpenseController,
+    CreateUserController,
+    DeleteUserController,
+    RecoverUserPasswordController,
+    RefreshUserTokenController,
+    SendUserEmailUpdateLinkController,
+    SendUserPasswordRecoveryLinkController,
+    UpdateUserEmailController,
+    UpdateUserPasswordController,
+    UpdateUsernameController,
+    UserLoginController,
+    VerifyUserEmailController,
+    ExecuteChargeToExpiredSubscriptionsController,
+    GetUserSubscriptionController,
+    ManageSubscriptionRenewalController,
+    UpdateSubscriptionRenewalStatusController,
+    UpdateSubscriptionController,
+    GetPlansController,
+    GetUserPlanController,
+    CreatePaymentMethodController,
+    DeletePaymentMethodController,
+    GetUserPaymentMethodController,
+    UpdatePaymentMethodNameController,
+    UpdatePaymentMethodTokenController,
+    CreateExtractController,
+    SendNotificationOfSubscriptionThatAreComingDueController,
+    SetFreePlanForNonRenewableSubscriptionsController,
+    AuthenticateUserMiddleware,
+    BasicAuthenticationMiddleware
+} from "@/layers/presentation";
+
+import {
+    createUserUseCase,
+    deleteUserUseCase,
+    recoverUserPasswordUseCase,
+    refreshUserTokenUseCase,
+    sendUserEmailUpdateLinkUseCase,
+    sendUserPasswordRecoveryLinkUseCase,
+    updateUserEmailUseCase,
+    updateUserPasswordUseCase,
+    updateUsernameUseCase,
+    userLoginUseCase,
+    verifyUserEmailUseCase,
+    executeChargeToExpiredSubscriptionsUseCase,
+    getActiveNonRenewableSubscriptionsUseCase,
+    getUserSubscriptionUseCase,
+    manageSubscriptionRenewalUseCase,
+    sendNotificationOfSubscriptionThatAreComingDueUseCase,
+    updateSubscriptionUseCase,
+    updateSubscriptionRenewalStatusUseCase,
+    getPlansUseCase,
+    getUserPlanUseCase,
+    createPaymentMethodUseCase,
+    deletePaymentMethodUseCase,
+    getUserPaymentMethodUseCase,
+    updatePaymentMethodNameUseCase,
+    updatePaymentMethodTokenUseCase,
+    createExtractUseCase,
+    deleteExpiredExtractsUseCase,
+    getUserExtractsUseCase,
+    createExpenseUseCase,
+    deleteExpenseUseCase,
+    expenseUndoPaymentUseCase,
+    getUserExpensesUseCase,
+    payExpenseUseCase,
+    sendNotificationOfExpensesThatAreComingDueUseCase,
+    updateExpenseUseCase,
+    updatePreviousMonthPaidExpensesToUnpaidUseCase
+} from "./use-cases";
+import { securityAdapter, winstonAdapter } from "./external";
+
+export const createUserController
+    = new CreateUserController(createUserUseCase, winstonAdapter);
+
+export const deleteUserController 
+    = new DeleteUserController(deleteUserUseCase, winstonAdapter);
+
+export const recoverUserPasswordController 
+    = new RecoverUserPasswordController(recoverUserPasswordUseCase, winstonAdapter);
+
+export const refreshUserTokenController 
+    = new RefreshUserTokenController(refreshUserTokenUseCase, winstonAdapter);
+
+export const sendUserEmailUpdateLinkController
+    = new SendUserEmailUpdateLinkController(sendUserEmailUpdateLinkUseCase, winstonAdapter);
+
+export const sendUserPasswordRecoveryLinkController
+    = new SendUserPasswordRecoveryLinkController(sendUserPasswordRecoveryLinkUseCase, winstonAdapter);
+
+export const updateUserEmailController 
+    = new UpdateUserEmailController(updateUserEmailUseCase, winstonAdapter);
+
+export const updateUserPasswordController 
+    = new UpdateUserPasswordController(updateUserPasswordUseCase, winstonAdapter);
+
+export const updateUsernameController 
+    = new UpdateUsernameController(updateUsernameUseCase, winstonAdapter);
+
+export const userLoginController 
+    = new UserLoginController(userLoginUseCase, winstonAdapter);
+
+export const verifyUserEmailController 
+    = new VerifyUserEmailController(verifyUserEmailUseCase, winstonAdapter);
+
+
+export const executeChargeToExpiredSubscriptionsController
+    = new ExecuteChargeToExpiredSubscriptionsController(executeChargeToExpiredSubscriptionsUseCase, winstonAdapter);
+
+export const getUserSubscriptionController
+    = new GetUserSubscriptionController(getUserSubscriptionUseCase, winstonAdapter);
+
+export const manageSubscriptionRenewalController
+    = new ManageSubscriptionRenewalController(manageSubscriptionRenewalUseCase, winstonAdapter);
+
+export const sendNotificationOfSubscriptionThatAreComingDueController
+    = new SendNotificationOfSubscriptionThatAreComingDueController(sendNotificationOfSubscriptionThatAreComingDueUseCase, winstonAdapter);
+
+export const updateSubscriptionController
+    = new UpdateSubscriptionController(updateSubscriptionUseCase, winstonAdapter);
+
+export const updateSubscriptionRenewalStatusController
+    = new UpdateSubscriptionRenewalStatusController(updateSubscriptionRenewalStatusUseCase, winstonAdapter);
+
+export const setFreePlanForNonRenewableSubscriptionsController 
+    = new SetFreePlanForNonRenewableSubscriptionsController(
+        getActiveNonRenewableSubscriptionsUseCase, 
+        manageSubscriptionRenewalUseCase, 
+        winstonAdapter
+);
+
+export const getPlansController 
+    = new GetPlansController(getPlansUseCase, winstonAdapter);
+
+export const getUserPlanController 
+    = new GetUserPlanController(getUserPlanUseCase, winstonAdapter);
+
+
+export const createPaymentMethodController 
+    = new CreatePaymentMethodController(createPaymentMethodUseCase, winstonAdapter);
+
+export const deletePaymentMethodController 
+    = new DeletePaymentMethodController(deletePaymentMethodUseCase, winstonAdapter);
+
+export const getUserPaymentMethodController 
+    = new GetUserPaymentMethodController(getUserPaymentMethodUseCase, winstonAdapter);
+
+export const updatePaymentMethodNameController 
+    = new UpdatePaymentMethodNameController(updatePaymentMethodNameUseCase, winstonAdapter);
+
+export const updatePaymentMethodTokenController 
+    = new UpdatePaymentMethodTokenController(updatePaymentMethodTokenUseCase, winstonAdapter);
+
+
+export const createExtractController 
+    = new CreateExtractController(createExtractUseCase, winstonAdapter);
+
+export const deleteExpiredExtractsController 
+    = new DeleteExpiredExtractsController(deleteExpiredExtractsUseCase, winstonAdapter);
+
+export const getUserExtractsController 
+    = new GetUserExtractsController(getUserExtractsUseCase, winstonAdapter);
+
+
+export const createExpenseController =
+    new CreateExpenseController(createExpenseUseCase, winstonAdapter);
+
+export const deleteExpenseController =
+    new DeleteExpenseController(deleteExpenseUseCase, winstonAdapter);
+
+export const expenseUndoPaymentController =
+    new ExpenseUndoPaymentController(expenseUndoPaymentUseCase, winstonAdapter);
+
+export const getUserExpensesController =
+    new GetUserExpensesController(getUserExpensesUseCase, winstonAdapter);
+
+export const payExpenseController =
+    new PayExpenseController(payExpenseUseCase, winstonAdapter);
+
+export const sendNotificationOfExpensesThatAreComingDueController =
+    new SendNotificationOfExpensesThatAreComingDueController(sendNotificationOfExpensesThatAreComingDueUseCase, winstonAdapter);
+
+export const updateExpenseController =
+    new UpdateExpenseController(updateExpenseUseCase, winstonAdapter);
+
+export const updatePreviousMonthPaidExpensesToUnpaidController =
+    new UpdatePreviousMonthPaidExpensesToUnpaidController(updatePreviousMonthPaidExpensesToUnpaidUseCase, winstonAdapter);
+
+    
+export const authenticationUserMiddleware =  
+    new AuthenticateUserMiddleware(securityAdapter);
+
+export const basicAuthenticationMiddleware = 
+    new BasicAuthenticationMiddleware(securityAdapter);

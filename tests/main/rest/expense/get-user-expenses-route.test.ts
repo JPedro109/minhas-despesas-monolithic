723f2c2594d0b72be3d8087e11b0595f9dcc3def
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-01-05 00:52:46 -0300

    test: add get user expenses route test

---
+++ tests/main/rest/expense/get-user-expenses-route.test.ts
@@ -0,0 +1,21 @@
+jest.setTimeout(10000);
+
+import { setupServer } from "@/main/setup-server";
+import { loginRest, setup } from "../../__mocks__";
+import request from "supertest";
+
+describe("/api/expenses - GET", () => {
+
+    setup();
+
+    test("Should return a list of expenses successfully", async () => {
+        const token = await loginRest("email-with-plan-gold-and-with-expenses-and-extracts@test.com");
+
+        const response = await request(setupServer())
+            .get("/api/expenses")
+            .set("User-Agent", "Supertest-Client/1.0")
+            .set("authorization", `Bearer ${token.accessToken}`);
+
+        expect(response.statusCode).toBe(200);
+    });
+});

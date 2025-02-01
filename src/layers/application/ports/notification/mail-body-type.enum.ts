fc33b535b61c85ee19dc752996c07ba5e891ef95
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-01 02:08:40 -0300

    refactor: add new mail body type in enum

--- src/layers/application/ports/notification/mail-body-type.enum.ts
+++ src/layers/application/ports/notification/mail-body-type.enum.ts
@@ -4,4 +4,5 @@ export enum MailBodyTypeEnum {
     RecoveryUserPasswordBody = "recovery_user_password_body",
     NotifyExpenseThatIsDueBody = "notify_expense_that_is_due_body",
     NotifySubscriptionThatIsDueBody = "notify_subscription_that_is_due_body",
+    NotifySubscriptionPaymentFailureBody = "notify_subscription_payment_failure_body",
 }
